441f7df359a1f1ea1679a62997bb3fdb13d91f66
Giuliana Rojas <giulirojas@gmail.com>
2025-07-23 23:39:51 -0300

    crear clase 2

---
+++ clase 2/variables.py
@@ -0,0 +1,24 @@
+print("Hola Mundo desde la Unida")
+entero=10
+flotante=10.5
+cadena="hola mundo"
+booleano=True
+print(entero, flotante,cadena, booleano)
+print(type(entero))
+print(type(flotante))
+print(type(cadena))
+print(type(booleano))
+
+print("sumando dos numeros")
+#operaciones basicas con variables
+#suma, rsta, muulplicacion, division
+a=10
+b=20
+print("suma de dos numeros", a + b)
+print("restando dos numeros")
+print("resta de dos numeros", a - b)
+print("multiplicando dos numeros")
+print("multiplicacion de dos numeros",a * b)
+b=2
+print("dividiendo dos numeros")
+print("division de dos numeros", a / b)
