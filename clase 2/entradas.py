4905a032b29ff9cce66335212fb9bd8f4ff6a475
Giuliana Rojas <giulirojas@gmail.com>
2025-07-24 00:58:08 -0300

    entradas.py

---
+++ clase 2/entradas.py
@@ -0,0 +1,10 @@
+'''
+entradas de datos por teclados
+'''
+
+
+cadena = input("ingrese nombre de su proyecto: ")
+print(f"el nombre de su proyecto es: {cadena}")
+
+numero = int(input("¿que version es? "))
+print(f"la version de su proyecto es: {numero+1}")
